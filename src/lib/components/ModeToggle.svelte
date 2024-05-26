8ce433b2c7fbabfe8eb59561ba633257bb827a99
KlausMass <massinemouha@zohomail.com>
2024-05-26 20:36:25 +0100

    boiler plate of the project

---
+++ src/lib/components/ModeToggle.svelte
@@ -0,0 +1,17 @@
+<script>
+	import { toggleMode } from 'mode-watcher';
+	import Icon from '@iconify/svelte';
+	import Button from './ui/button/button.svelte';
+</script>
+
+<Button on:click={toggleMode} variant="outline" size="icon">
+	<Icon
+		icon="material-symbols:sunny"
+		class="h-[1.2rem] w-[1.2rem] rotate-0 scale-100 transition-all dark:-rotate-90 dark:scale-0"
+	/>
+	<Icon
+		icon="material-symbols:dark-mode"
+		class="absolute h-[1.2rem] w-[1.2rem] rotate-90 scale-0 transition-all dark:rotate-0 dark:scale-100"
+	/>
+	<span class="sr-only">Toggle theme</span>
+</Button>
---
+++ src/routes/+layout.svelte
@@ -0,0 +1,29 @@
+<script lang="ts">
+	import { ModeWatcher } from 'mode-watcher';
+	import '../app.css';
+	import ModeToggle from '$lib/components/ModeToggle.svelte';
+	import Icon from '@iconify/svelte';
+</script>
+
+<ModeWatcher />
+
+<header
+	class="sticky top-0 z-50 w-full border-b border-border/40 bg-background/95 backdrop-blur supports-[backdrop-filter]:bg-background/60"
+>
+	<div class="container flex h-14 max-w-screen-2xl items-center">
+		<div class="flex">
+			<span class="text-xl font-bold sm:inline-block">Open Soap Calculator</span>
+		</div>
+		<div class="flex flex-1 items-center justify-between space-x-2 md:justify-end">
+			<ModeToggle />
+			<a href="">
+				<Icon icon="mdi:github" />
+			</a>
+		</div>
+	</div>
+</header>
+<main class="container py-8 md:py-12">
+	<slot></slot>
+</main>
+
+<style></style>
